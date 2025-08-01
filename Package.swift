a4fe41862f98ca6f1fbbec0d2f7e2576b7a7a125
Greg ORIOL <greg@gregoriol.net>
2025-08-01 11:38:33 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version: 6.1
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Source"
+        ),
+        .testTarget(
+            name: "LocksmithTests",
+            dependencies: ["Locksmith"]
+        ),
+    ]
 )
